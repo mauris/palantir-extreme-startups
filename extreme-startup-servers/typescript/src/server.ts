4d69edf884978f7ce99204e2013c7b3dbbb3d93c
Sam-Mauris Yong <mauris@hotmail.sg>
2017-11-16 21:46:54 +0000

    fixing problems in server.ts

--- extreme-startup-servers/typescript/src/server.ts
+++ extreme-startup-servers/typescript/src/server.ts
@@ -4,17 +4,17 @@ import {respondTo} from "./respondTo";
 
 const app = express();
 
-app.get("/", (request: express.Request, response: express.Response) => {
-    const query = request.query["q"];
-    console.log("A request has arrived:");
-    console.log(query);
+app.get("/", (request: any, response: any) => {
+  const query = request.query["q"];
+  console.log("A request has arrived:");
+  console.log(query);
 
-    const answer = respondTo(query);
-    response.end(answer);
+  const answer = respondTo(query);
+  console.log('Our answer is: ' + answer);
+  response.end(answer);
 });
-
 
 const PORT = 1337;
 app.listen(PORT, () => {
-    console.log(`Typescript Extreme Startup Server running on port ${PORT}`);
+  console.log(`Typescript Extreme Startup Server running on port ${PORT}`);
 });
